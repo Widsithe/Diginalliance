baba72ec96ee11fa5395dd194747e6143f74e133
Unknown <trecy.christine.fournier@gmail.com>
2018-06-20 15:52:21 +0200

    site

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,21 @@
 $('.carousel').carousel({
   interval: 10000
-})
+});
+
+function envoi() {
+ // par défaut, pas d'envoi
+ document.diginaliance.action="";
+ // recherche de @ dans l'adresse
+ var address=window.document.diginaliance.adr.value;
+ var pos=address.indexOf("@");
+ if (address=="") {
+  alert("Vous devez donner une adresse e-mail.");
+ }
+ else {
+  if (pos==-1 || pos==0 || pos==address.length-1) {
+   alert("Une adresse e-mail a le format nom@domaine");
+  }
+  else // tout va bien, on envoie le formulaire
+   document.formu.action='mailto:trecy.christine.fournier@gmail.com?subject="message"';
+ }
+
